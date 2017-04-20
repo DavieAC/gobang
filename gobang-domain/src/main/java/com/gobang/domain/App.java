41bcab49d8699da5dc2fc505a22330b2015b0b7a
daishiji <davieac@foxmail.com>
2017-04-20 23:28:42 +0800

    1. 添加了一个新的module：gobang-domain 2. 添加了对应的依赖

---
+++ gobang-domain/src/main/java/com/gobang/domain/App.java
@@ -0,0 +1,13 @@
+package com.gobang.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
