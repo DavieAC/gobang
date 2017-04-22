a9b26b0d48305a68372be62070c448bb9d4c1aa2
daishiji <davieac@foxmail.com>
2017-04-22 18:19:37 +0800

    1. 修改了Max层和Min层的函数逻辑 2. 合并了bestMove和Move

--- gobang-domain/src/main/java/com/gobang/domain/ai/Move.java
+++ gobang-domain/src/main/java/com/gobang/domain/ai/Move.java
@@ -2,33 +2,61 @@ package com.gobang.domain.ai;
 
 public class Move {
 
+    /**
+     * 一个落子的X坐标
+     */
     private int x;
 
+    /**
+     * 一个落子的Y坐标
+     */
     private int y;
-    
+
+    /**
+     * 一个落子的分数，在递归函数中需要用到
+     */
+    private int score;
+
     public Move() {
-        
+
     }
-    
+
     public Move(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
+
+    public Move(int x, int y, int score) {
+        this.x = x;
+        this.y = y;
+        this.score = score;
+    }
+
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public Move setX(int x) {
         this.x = x;
+        return this;
     }
 
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
+    public Move setY(int y) {
         this.y = y;
+        return this;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Move setScore(int score) {
+        this.score = score;
+        return this;
     }
 
 }
--- gobang-service/src/main/java/com/gobang/ai/impl/SelectorImpl.java
+++ gobang-service/src/main/java/com/gobang/ai/impl/SelectorImpl.java
@@ -1,14 +1,11 @@
 package com.gobang.ai.impl;
 
-import java.util.ArrayList;
-
 import javax.annotation.Resource;
 
 import com.gobang.ai.interfaces.Evaluator;
 import com.gobang.ai.interfaces.MovePicker;
 import com.gobang.ai.interfaces.Selector;
 import com.gobang.constant.Constant;
-import com.gobang.domain.ai.BestMove;
 import com.gobang.domain.ai.Move;
 import com.gobang.tools.ArrayTool;
 
@@ -33,58 +30,75 @@ public class SelectorImpl implements Selector {
         return 0;
     }
 
-    private BestMove getMaxScore(int[][] chessInfo, int count) {
+    private Move getMaxScore(int[][] chessInfo, int count) {
 
         // 用来记录当前层的最佳落子
-        BestMove curBestMove = new BestMove();
+        Move curBestMove = new Move();
 
         // 如果计算到最后一层（此时没有x坐标和y坐标）
         if (count == 0) {
             return curBestMove.setScore(evaluator.getAllScore(chessInfo));
         }
 
-        int curMaxScore = -1;
+        int curMaxScore = Integer.MIN_VALUE;
 
-        // 开始遍历所有有意义的落子
-        ArrayList<Move> moves = movePicker.getValuableMove(chessInfo);
-        for (Move move : moves) {
-            int[][] newChessInfo = ArrayTool.copyOf(chessInfo);
+        // 新的棋局
+        int[][] newChessInfo = ArrayTool.copyOf(chessInfo);
+        // 开始遍历所有有意义的落子,往下层递归
+        for (Move move : movePicker.getValuableMove(newChessInfo)) {
+
+            // 在新的棋局上落子
             newChessInfo[move.getX()][move.getY()] = Constant.BLACK;
-            int score = evaluator.getAllScore(newChessInfo);
-            if (score > curMaxScore) {
+
+            Move nextBesMove = getMinScore(newChessInfo, count - 1);
+            if (nextBesMove.getScore() > curMaxScore) {
+                curMaxScore = move.getScore();
+
                 curBestMove.setX(move.getX());
                 curBestMove.setY(move.getY());
-                curBestMove.setScore(score);
+                curBestMove.setScore(nextBesMove.getScore());
             }
+
+            // 还原
+            newChessInfo[move.getX()][move.getY()] = Constant.BLANK;
         }
         return curBestMove;
     }
 
-    private BestMove getMinScore(int[][] chessInfo, int count) {
+    private Move getMinScore(int[][] chessInfo, int count) {
 
         // 用来记录当前层的最佳落子
-        BestMove curBestMove = new BestMove();
+        Move curBestMove = new Move();
 
         // 如果计算到最后一层（此时没有x坐标和y坐标）
         if (count == 0) {
             return curBestMove.setScore(evaluator.getAllScore(chessInfo));
         }
 
-        int curScore = Integer.MAX_VALUE;
+        int curMinScore = Integer.MAX_VALUE;
 
-        // 开始遍历所有有意义的落子
-        ArrayList<Move> moves = movePicker.getValuableMove(chessInfo);
-        for (Move move : moves) {
-            int[][] newChessInfo = ArrayTool.copyOf(chessInfo);
+        // 新的棋局
+        int[][] newChessInfo = ArrayTool.copyOf(chessInfo);
+        // 开始遍历所有有意义的落子,往下层递归
+        for (Move move : movePicker.getValuableMove(newChessInfo)) {
+
+            // 在新的棋局上落子
             newChessInfo[move.getX()][move.getY()] = Constant.WHITE;
-            int score = evaluator.getAllScore(newChessInfo);
-            if (score < curScore) {
+
+            Move nextBesMove = getMaxScore(newChessInfo, count - 1);
+            if (nextBesMove.getScore() < curMinScore) {
+                curMinScore = move.getScore();
+
                 curBestMove.setX(move.getX());
                 curBestMove.setY(move.getY());
-                curBestMove.setScore(score);
+                curBestMove.setScore(nextBesMove.getScore());
             }
+
+            // 还原
+            newChessInfo[move.getX()][move.getY()] = Constant.BLANK;
         }
         return curBestMove;
     }
+
 
 }
